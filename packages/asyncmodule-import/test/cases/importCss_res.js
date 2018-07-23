f0a699f37a800dbec84bb5656313dfc3cbe4cbda
shizhipeng <hzshzhipeng@corp.netease.com>
2018-07-23 12:37:09 +0800

    chore: async import add babelrc test case

---
+++ packages/asyncmodule-import/test/cases/importCss_res.js
@@ -0,0 +1,9 @@
+import AsyncModule from 'react-asyncmodule';
+import ImportCss from 'babel-plugin-asyncmodule-import/importcss';
+const AsyncComponent = AsyncModule({
+    delay: 300
+});
+const Home = AsyncComponent({
+    load: () => Promise.all([import( /*webpackChunkName: "home"*/'./views/home'), ImportCss('home')]).then(jsprim => jsprim[0]),
+    resolveWeak: () => require.resolveWeak('./views/home')
+});
